f3aedee14daf317663dfac26ea7ad258ed588230
salam <salam.tamimi99@gmail.com>
2023-11-30 03:42:13 +0300

    salam

--- salamtak/resources/views/pages/doctor-single.blade.php
+++ salamtak/resources/views/pages/doctor-single.blade.php
@@ -47,6 +47,8 @@
     
         .review-container {
             border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
             padding: 2%;
             width: 50%;
             margin-bottom: 20px;
@@ -64,6 +66,11 @@
             border-radius: 50%;
             margin-right: 10px;
         }
+        /* .star-svg {
+        width: 25px;
+        height: 25px;
+        margin-right: 3px;
+    } */
     </style>
 </head>
 
@@ -425,7 +432,7 @@
 
 <div class="reviews-container">
     <div class="title">
-        <h2 style="color: var(--dark);">تقييمات الطبيب</h2>
+        <h2 style="color: var(--primary);">تقييمات الطبيب</h2>
         <div class="underline"></div>
     </div>
     {{-- Reviews start --}}
@@ -433,8 +440,47 @@
         @if ($review)
             <div class="review-container">
                 <p>{{ $review->comment }}</p>
-                <p>{{ $review->review }}</p>
-                
+                {{-- <p>{{ $review->review }}</p> --}}
+                {{-- Display stars based on rating --}}
+                @php
+                    $rating = $review->review;
+                    $filledStars = floor($rating);
+                    $halfStar = ceil($rating - $filledStars);
+                    $emptyStars = 5 - $filledStars - $halfStar;
+                @endphp
+
+                <div class="star-container">
+                    @for ($i = 0; $i < $filledStars; $i++)
+                    <svg width="30px" height="30px" viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#FAD97F">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path d="M923.2 429.6H608l-97.6-304-97.6 304H97.6l256 185.6L256 917.6l256-187.2 256 187.2-100.8-302.4z"stroke="#CCCCCC"></path>
+                        </g>
+                    </svg>
+                    @endfor
+
+                    @if ($halfStar)
+                    <svg fill="#FAD97F" width="28px" height="28px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier"> <title>star-half-stroke-filled</title> 
+                            <path d="M30.383 12.699c-0.1-0.303-0.381-0.519-0.713-0.519-0 0-0 0-0 0h-9.898l-3.059-9.412c-0.1-0.303-0.381-0.518-0.712-0.518-0.083 0-0.163 0.014-0.238 0.039l0.005-0.002c-0.226 0.078-0.399 0.256-0.468 0.48l-0.001 0.005-0.012-0.004-3.059 9.412h-9.897c-0.414 0-0.749 0.336-0.749 0.75 0 0.248 0.121 0.469 0.307 0.605l0.002 0.001 8.007 5.818-3.059 9.412c-0.023 0.069-0.037 0.149-0.037 0.232 0 0.414 0.336 0.75 0.75 0.75 0.165 0 0.318-0.053 0.442-0.144l-0.002 0.001 8.008-5.819 8.006 5.819c0.122 0.090 0.275 0.144 0.441 0.144 0.414 0 0.75-0.336 0.75-0.75 0-0.083-0.014-0.164-0.039-0.239l0.002 0.005-3.059-9.412 8.010-5.818c0.188-0.138 0.308-0.357 0.308-0.605 0-0.083-0.014-0.163-0.038-0.238l0.002 0.005zM20.779 18.461c-0.188 0.138-0.309 0.358-0.309 0.607 0 0.083 0.014 0.163 0.039 0.238l-0.002-0.005 2.514 7.736-6.581-4.783c-0.116-0.080-0.259-0.128-0.414-0.128-0.009 0-0.018 0-0.028 0l0.001-0v-16.701l2.514 7.737c0.1 0.303 0.381 0.519 0.713 0.519 0 0 0 0 0 0h8.135z"></path> 
+                        </g>
+                    </svg> 
+                    @endif
+
+                    @for ($i = 0; $i < $emptyStars; $i++)
+                    <svg width="28px" height="28px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <g id="icomoon-ignore"></g>
+                            <path d="M19.38 12.803l-3.38-10.398-3.381 10.398h-11.013l8.925 6.397-3.427 10.395 8.896-6.448 8.895 6.448-3.426-10.395 8.925-6.397h-11.014zM20.457 19.534l2.394 7.261-6.85-4.965-6.851 4.965 2.64-8.005-0.637-0.456-6.228-4.464h8.471l2.606-8.016 2.605 8.016h8.471l-6.864 4.92 0.245 0.744z"stroke="#CCCCCC"></path>
+                        </g>
+                    </svg>
+                    @endfor
+                </div>
                 <div class="user-info">
                     <img src="{{ $review->appointments->user->image }}" alt="User Image" class="user-image">
                     <p>{{ $review->appointments->user->name }}</p>
