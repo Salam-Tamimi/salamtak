5e3e209b9f578e3a9fa20cbcaa52930bf4ab386d
salam <salam.tamimi99@gmail.com>
2023-12-01 14:45:05 +0300

    salam

--- salamtak/resources/views/hospital/pages/reviews-admin/index.blade.php
+++ salamtak/resources/views/hospital/pages/reviews-admin/index.blade.php
@@ -26,7 +26,7 @@
 								<div class="card-body">
 									<div class="table-responsive">
 										@if ($reviews->isNotEmpty())
-    <table>
+    <table class="table">
         <thead>
             <tr>
                 <th>اسم المريض</th>
